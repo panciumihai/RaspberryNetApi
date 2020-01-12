65e34cadb10adf746722e2c546c05295aae47d11
panciu_rusu_mihai <panciu.rusu.mihai@gmail.com>
2020-01-12 18:15:02 +0200

    Update api

--- RaspberryNetAPi/Controllers/TestController.cs
+++ RaspberryNetAPi/Controllers/TestController.cs
@@ -7,87 +7,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace RaspberryNetAPi.Controllers
 {
-    public class TestController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
     {
-        // GET: Test
-        public ActionResult Index()
+        [HttpGet]
+        public IActionResult Index()
         {
-            return View();
-        }
-
-        // GET: Test/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Test/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Test/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Test/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Test/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Test/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Test/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            var list = new List<string>();
+            list.Add("Bine ai venit pe Pantazon!");
+            list.Add("Mihai este seful");
+            return Ok(list);
         }
     }
 }
